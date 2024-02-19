fbe1476887105b66e2d7b386f0b382ba86f85aa5
BezerraC <cbezerraneto@gmail.com>
2024-02-19 18:40:31 -0300

    fix: readthedocs theme

--- docs/conf.py
+++ docs/conf.py
@@ -6,6 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import sphinx_rtd_theme
 import os
 import sys
 
